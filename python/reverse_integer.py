e98009d6e66d102d2effb8e0905e9f198aef4357
Solomon Belay <sofi2darling@gmail.com>
2026-08-25 21:44:33 +0300

    Use floor division for integer operations
    Fix integer division for reversing digits

--- python/reverse_integer.py
+++ python/reverse_integer.py
@@ -5,11 +5,11 @@ class Solution:
     if(x == 0 and copy == 0) return 0
     while( x != 0):
       digit = x % 10
-      x = x/10
+      x = x//10
       count++
     while( copy != 0):
       digit = copy % 10
       reverse = reverse + digit * 10**(count-1)
-      copy = copy / 10
+      copy = copy // 10
       count--
     return reverse
