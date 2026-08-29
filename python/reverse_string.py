d62339e029d12a701f6e7c7dd68f9d6849591e4e
Solomon Belay <sofi2darling@gmail.com>
2026-08-29 21:38:52 +0300

    Add reverse_string function to reverse a list

---
+++ python/reverse_string.py
@@ -0,0 +1,13 @@
+class Solution:
+  def reverse_string(self, s):
+    i = 0
+    j = len(s) - 1
+    while(i < j):
+      temp = s[i]
+      s[i] = s[j]
+      s[j] = temp
+      i += 1
+      j -= 1
+    return s
+    
+     
